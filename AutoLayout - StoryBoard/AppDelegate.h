19815af4fbcbec842e0eadd970efc12195d9d643
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-20 19:13:12 -0800

    Initial Commit

---
+++ AutoLayout - StoryBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout - StoryBoard
+//
+//  Created by Chris Dean on 2018-02-20.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
